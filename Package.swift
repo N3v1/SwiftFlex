9354e1f38770a459e9f07e6df721b2fb7d5490ae
N3v1 <n3v1010@gmail.com>
2024-01-29 21:33:46 +0100

    [Chore]: Config Package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,42 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFlex",
+    platforms: [
+            .iOS(.v17),
+            .macOS(.v14),
+            .tvOS(.v17),
+            .watchOS(.v10)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftFlex",
-            targets: ["SwiftFlex"]),
+            targets: ["_SwiftFlex", "SwiftFlex"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftFlex"),
-        .testTarget(
-            name: "SwiftFlexTests",
-            dependencies: ["SwiftFlex"]),
+            name: "_SwiftFlex",
+            dependencies: [],
+            swiftSettings: [
+                .unsafeFlags([
+                    "-enable-library-evolution", "-suppress-warnings"
+                ])
+            ]
+        ),
+        .target(
+            name: "SwiftFlex",
+            dependencies: ["_SwiftFlex"],
+            swiftSettings: [
+                .unsafeFlags([
+                    "-enable-library-evolution",
+                    "-Xfrontend",
+                    "-disable-typo-correction",
+                    "-Xfrontend",
+                    "-disable-verify-exclusivity",
+                ])
+            ]
+        )
     ]
 )
---
+++ Sources/_SwiftFlex/module.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nevio Hirani on 29.01.24.
+//
+
+@_exported import SwiftUI
