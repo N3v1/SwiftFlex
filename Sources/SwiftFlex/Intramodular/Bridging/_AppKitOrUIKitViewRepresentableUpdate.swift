ba351127b7febcc335cfc08be46d9bbb15d31076
N3v1 <n3v1010@gmail.com>
2024-01-30 13:12:31 +0100

    [Chore]: Bridging Layers

---
+++ Sources/SwiftFlex/Intramodular/Bridging/_AppKitOrUIKitViewRepresentableUpdate.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Copyright (c) 2024.
+//
+
+import Swift
+import SwiftUI
+
+struct _AppKitOrUIKitViewRepresentableUpdate: Hashable {
+    let generation: AnyHashable = UUID()
+
+    init() {
+
+    }
+}
